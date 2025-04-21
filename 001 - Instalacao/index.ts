6b1b7db53ad472c5e4b50110250648304b4b9cd3
Fernando Vargas <fernando.assis@unimedpr.com.br>
2025-04-21 16:32:39 -0300

    001 - Instalacao

---
+++ 001 - Instalacao/index.ts
@@ -0,0 +1,19 @@
+// Typescript
+// npm install -g typescript
+
+// tsc --init
+
+// Plugin: Code Runner
+// npm i -g ts-node
+// ctrl + alt + n
+
+// compilar codigo TS para JS -> tsc index.ts
+
+console.log("npm install -g typescript");
+
+const pedido = (pedido: string) => {
+return `Deu certo o pedido ${pedido}`;
+};
+
+console.log(pedido("1234"));
+
