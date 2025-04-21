acacc176f2efa38e10a75fe2bc248ffa90e5ee0e
Fernando Vargas <fernando.assis@unimedpr.com.br>
2025-04-21 16:32:39 -0300

    002 - Instalacao

---
+++ 001 - Instalacao/index.js
@@ -0,0 +1,13 @@
+"use strict";
+// Typescript
+// npm install -g typescript
+// tsc --init
+// Plugin: Code Runner
+// npm i -g ts-node
+// ctrl + alt + n
+// compilar codigo TS para JS -> tsc index.ts
+console.log("npm install -g typescript");
+
+// Atenção...Utilizamos o .ts para dar super poderes ao JavaScript como recursos, tipagem de variáveis, 
+// depuração só que o código que vai rodar no servidor estará em JavaScript. 
+// Quando compilamos o TypeScript no final é gerado JavaScript.
